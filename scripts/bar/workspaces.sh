9b4410930894d0b5d7fbeabc22dd350b26ce11e1
Wachamuli <josemrr27@gmail.com>
2024-07-09 18:53:41 -0400

    Better images folder access and power menu

--- scripts/bar/workspaces.sh
+++ scripts/bar/workspaces.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Use grep and awk to filter and extract workspace IDs
+focused_workspace=$(hyprctl activeworkspace | grep -m 1 "ID" | awk '{print $3}')
+
+
 workspace_ids=$(hyprctl workspaces | grep 'workspace ID' | awk '{print $3}' | tr -d '(' | tr -d ')')
 
 # Sort the IDs and convert them into a string formatted as an array in one line
 sorted_ids=$(echo "$workspace_ids" | tr ' ' '\n' | sort -n | tr '\n' ',' | sed 's/,$//')
-# Format as an array string
-active_workspace_ids_array="[$sorted_ids]"
 
-focused_workspace=$(hyprctl activeworkspace | grep -m 1 "ID" | awk '{print $3}')
+max_id=$(echo "$workspace_ids" | tr ' ' '\n' | sort -n | tail -n 1)
+
+# Calculate max_id + 1
+next_id=$((max_id + 1))
+
+# Format as an array string
+active_workspace_ids_array="[$sorted_ids,$next_id]"
 
 echo "{ \"focused\": ${focused_workspace}, \"actives\": ${active_workspace_ids_array} }"
