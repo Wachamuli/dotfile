ac9593cd65b3ea8c8474d3f91610258b092837d5
Wachamuli <josemrr27@gmail.com>
2024-07-12 19:27:19 -0400

    Yuck code is no longer in notification script.

--- scripts/bar/notifications.py
+++ scripts/bar/notifications.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import collections
+import json
 import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
@@ -9,7 +11,7 @@ import time
 
 def main():
     DBusGMainLoop(set_as_default=True)
-    _ = NotificationServer()
+    NotificationServer()
     mainloop = GLib.MainLoop()
     mainloop.run()
 
@@ -20,73 +22,47 @@ class Notification:
     summary: str
     body: str
     app_icon: str
-    
+
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
 
 class NotificationManager:
-    notifications: list[Notification] = []
-
-    @staticmethod
-    def remove_notification(notification: Notification):
-        time.sleep(10)
-        NotificationManager.notifications.remove(notification)
-        NotificationManager.show_notifications()
-
-    @staticmethod
-    def add_notification(notification: Notification):
-        NotificationManager.notifications.insert(0, notification)
-        NotificationManager.show_notifications()
-        timer_thread = threading.Thread(target=NotificationManager.remove_notification, args=(notification,))
-        timer_thread.start()
+    notifications: collections.deque[Notification] = collections.deque([], maxlen=5)
 
     @staticmethod
     def show_notifications():
-        notification = ""
-        
-        for data in NotificationManager.notifications:
-            notification += fr"""
-            (box
-                :class "notification-container"
-                :width 500
-                :spacing 20
-                :space-evenly false
-                (image :image-width 80 :image-height 80 :path "{data.app_icon or '${images_folder + "bell.svg"}'}")
-                (box
-                    :orientation "v"
-                    :space-evenly false
-                    :valign "center"
-                    :hexpand true
-                    (label :halign "start" :class "app-name" :limit-width 100 :text "{data.app_name or "Incoming notification"}")
-                    (label :halign "start" :class "summary" :limit-width 100 :text "{data.summary}")
-                    (label :halign "start" :limit-width 100 :text "{data.body}")
-                )
-            )
-            """
+        print(list(NotificationManager.notifications), flush=True)
 
-        notification = notification.replace("\n", " ")
-        print(fr'''(box :orientation "v" :spacing 20 {notification})''', flush=True)
+    @staticmethod
+    def add_notification(notification: Notification):
+        NotificationManager.notifications.appendleft(notification)
+        NotificationManager.show_notifications()
+        timer_thread = threading.Thread(target=NotificationManager.remove_notification)
+        timer_thread.start()
+
+    @staticmethod
+    def remove_notification():
+        time.sleep(10)
+        NotificationManager.notifications.pop()
+        NotificationManager.show_notifications()
 
 
 class NotificationServer(dbus.service.Object):
     def __init__(self):
-        bus_name = dbus.service.BusName('org.freedesktop.Notifications', bus=dbus.SessionBus())
-        dbus.service.Object.__init__(self, bus_name, '/org/freedesktop/Notifications')
+        bus_name = dbus.service.BusName("org.freedesktop.Notifications", bus=dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, "/org/freedesktop/Notifications")
 
-    @dbus.service.method('org.freedesktop.Notifications', out_signature='ssss')
+    @dbus.service.method("org.freedesktop.Notifications", out_signature="ssss")
     def GetServerInformation(self):
         return ("Custom Notification Server", "ExampleNS", "1.0", "1.2")
 
-    @dbus.service.method('org.freedesktop.Notifications', in_signature='susssasa{ss}i', out_signature='u')
+    @dbus.service.method("org.freedesktop.Notifications", in_signature="susssasa{ss}i", out_signature="u")
     def Notify(self, app_name, replaces_id, app_icon, summary, body, actions, hints, timeout):
-        notification = Notification(
-            app_name,
-            summary,
-            body,
-            app_icon
-        )
+        notification = Notification(app_name, summary, body, app_icon)
         NotificationManager.add_notification(notification)
         return 0
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
