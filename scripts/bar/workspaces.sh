562bc879b07fc8b38b0aae6c065fea6885ac42e3
Wachamuli <josemrr27@gmail.com>
2024-07-14 14:06:46 -0400

    Fixed: Special workspace is ignored

--- scripts/bar/workspaces.sh
+++ scripts/bar/workspaces.sh
@@ -4,7 +4,9 @@
 focused_workspace=$(hyprctl activeworkspace | grep -m 1 "ID" | awk '{print $3}')
 
 
-workspace_ids=$(hyprctl workspaces | grep 'workspace ID' | awk '{print $3}' | tr -d '(' | tr -d ')')
+workspace_ids=$(hyprctl workspaces | grep 'workspace ID' | awk '{print $3}' | tr -d '(' | tr -d ')' | awk '$1 >= 0')
+
+special_workspace=$(hyprctl workspaces | grep 'workspace ID' | awk '{print $3}' | tr -d '(' | tr -d ')' | awk '$1 < 0')
 
 # Sort the IDs and convert them into a string formatted as an array in one line
 sorted_ids=$(echo "$workspace_ids" | tr ' ' '\n' | sort -n | tr '\n' ',' | sed 's/,$//')
@@ -17,4 +19,13 @@ next_id=$((max_id + 1))
 # Format as an array string
 active_workspace_ids_array="[$sorted_ids,$next_id]"
 
-echo "{ \"focused\": ${focused_workspace}, \"actives\": ${active_workspace_ids_array} }"
+output=$(cat << EOM
+{ 
+    "focused": ${focused_workspace},
+    "actives": ${active_workspace_ids_array},
+    "special_workspace": "${special_workspace}"
+}
+EOM
+)
+
+echo $output
