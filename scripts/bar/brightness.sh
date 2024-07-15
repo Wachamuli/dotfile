e998c73d3ef93c23e0eabb6ed2a7126f7d87f93d
Wachamuli <josemrr27@gmail.com>
2024-07-15 17:11:41 -0400

    Fixed: brightness level is updated even when control menu is closed

--- scripts/bar/brightness.sh
+++ scripts/bar/brightness.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#Function to get the current brightness level
-get_current_brightness() {
-  brightnessctl g
-}
-
 # Initialize the last brightness level variable
-last_brightness=$(get_current_brightness)
+# last_brightness=$(get_current_brightness)
 
 # Start monitoring brightness level
 while true; do
   # Get the current brightness level
-  current_brightness=$(get_current_brightness)
+  current_brightness=$(brightnessctl g)
 
   # Print the brightness level if it has changed
   if [ "$current_brightness" != "$last_brightness" ]; then
