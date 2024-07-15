7a0dd6194362b2bd9593f99bf84a52eec9a85f0a
Wachamuli <josemrr27@gmail.com>
2024-07-15 18:38:37 -0400

    power_mode.sh cannot set the power mode anymore

--- scripts/bar/power_mode.sh
+++ scripts/bar/power_mode.sh
@@ -1,48 +1,15 @@
 #!/bin/bash
 
-get_power_mode () {
-    power_mode=$(powerprofilesctl get)
-    case "$power_mode" in
-        "power-saver")
-            echo "Power Saver"
-            ;;
-        "balanced")
-            echo "Balanced"
-            ;;
-        "performance")
-            echo "Performance"
-            ;;
-        *)
-        echo "Invalid value: select between saver, balanced or performance."
-            ;;
-    esac
-}
+power_mode=$(powerprofilesctl get)
 
-set_power_mode () {
-    case "$1" in
-        "saver")
-            powerprofilesctl set power-saver
-            ;;
-        "balanced")
-            powerprofilesctl set balanced
-            ;;
-        "performance")
-            powerprofilesctl set performance
-            ;;
-        *)
-        echo "Invalid value: select between saver, balanced or performance."
-            ;;
-    esac
-}
-
-if [[ "$1" == "get" ]]; then
-    get_power_mode
-elif [[ "$1" == "set" ]]; then
-    if [[ -z "$2" ]]; then
-        echo "Please provide a profile to set: saver, balanced, or performance."
-    else
-        set_power_mode "$2"
-    fi
-else
-    echo "Usage: $0 {get|set} [profile]"
-fi
+case "$power_mode" in
+    "power-saver")
+        echo "Power Saver"
+        ;;
+    "balanced")
+        echo "Balanced"
+        ;;
+    "performance")
+        echo "Performance"
+        ;;
+esac
